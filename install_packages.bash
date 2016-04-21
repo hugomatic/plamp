a9e7b65d1be3c46cd3998a047bbac549171c15de
hugomatic <hugo@hugomatic.ca>
2016-04-21 01:00:48 -0700

    plamp.js module with color

--- install_packages.bash
+++ install_packages.bash
@@ -3,7 +3,13 @@
 # install pimonori
 curl -sS get.pimoroni.com/unicornhat | bash
 
-apt-get install -y vim ipython python-dev python-pip python-bluez avahi-daemon
+# Using Debian, as root
+curl -sL https://deb.nodesource.com/setup_5.x | bash -
+apt-get install -y nodejs
+
+apt-get install -y vim ipython lsb-release git
+apt-get install -y python-dev python-pip python-bluez avahi-daemon
+
 pip install Flask
 pip install -U flask-cors
 pip install requests
