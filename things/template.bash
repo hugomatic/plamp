79c77b131a53a8a9cb33fe6639a4445dcf71d894
hugomatic <hugo@hugomatic.ca>
2026-04-15 19:41:07 -0700

    fix messages

--- things/template.bash
+++ things/template.bash
@@ -9,7 +9,7 @@ if [ -z "$part" ]; then
     echo "usage: $0 [part] whhere part is tbe name of the scad part to generate "
     exit -1
 else
-    echo "empty"
+    echo "Creating new OpenSCAD part"
 fi
 
 echo "Hello $part"
@@ -18,4 +18,4 @@ mkdir $part
 cp ./3d_template/generate.bash ./$1/generate.bash
 cp ./3d_template/cad.scad ./$1/$1.scad
 sed -i -e "s/__cad__name__/$1/g" ./$1/generate.bash
-rm ./$1/generate.bash-e
+rm -f ./$1/generate.bash-e
