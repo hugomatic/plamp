4e964684841f46cb6ead9b5a95109b2688120cf8
hugo <hugo@hugomatic.ca>
2026-07-16 11:36:34 -1000

    Add structured Pico health evidence

--- plamp/__init__.py
+++ plamp/__init__.py
@@ -1,5 +1,6 @@
 from plamp.camera import CameraError, capture_camera
 from plamp.locks import LockTimeout
+from plamp.pico_health import PicoHealth, PicoHealthError, failed_health, probe_pico
 from plamp.pico_transport import PicoClient, PicoCommandError, PicoFlashError, PicoReportTimeout, PicoUnavailable, pulse_gpio, request_report
 
-__all__ = ["CameraError", "LockTimeout", "PicoClient", "PicoCommandError", "PicoFlashError", "PicoReportTimeout", "PicoUnavailable", "capture_camera", "pulse_gpio", "request_report"]
+__all__ = ["CameraError", "LockTimeout", "PicoClient", "PicoCommandError", "PicoFlashError", "PicoHealth", "PicoHealthError", "PicoReportTimeout", "PicoUnavailable", "capture_camera", "failed_health", "probe_pico", "pulse_gpio", "request_report"]
